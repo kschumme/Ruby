ea6c16259ff1eec0b9aa47b49bb37ef11d2e0656
Karl SCHUMMER <kschumme@e3r11p18.42.fr>
2015-06-10 17:05:07 +0200

    good

--- TheFuckingApp/config/routes.rb
+++ TheFuckingApp/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get '/contact', :to => 'pages#contact'
-  get '/about', :to => 'pages#about'
-  get '/help', :to => 'pages#help'
+  #get '/contact', :to => 'pages#contact'
+  #get '/about', :to => 'pages#about'
+  #get '/help', :to => 'pages#help'
 
   root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
