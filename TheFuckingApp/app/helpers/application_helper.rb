a703ab7d48c9b6de3bab5ef4132755282c0d7d75
Karl SCHUMMER <kschumme@e1r7p15.42.fr>
2015-06-13 02:23:59 +0200

    good

--- TheFuckingApp/app/helpers/application_helper.rb
+++ TheFuckingApp/app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def logo
+		base_logo = image_tag("logo.png", :alt => "Application exemple", :class => "round")
+	end
+
 	#Me sauve ma vie
 	def titre 
 		base_titre = "Simple App du tutoriel Ruby on Rails"
